3d9769cf27b7a8a6a2916069f5848ab9c4a5bbc4
Meadowsdxd <58809634+Meadowsdxd@users.noreply.github.com>
2022-08-18 21:52:24 +0300

    Add Check

--- app/src/main/java/com/example/imtcalculator/more/Check.java
+++ app/src/main/java/com/example/imtcalculator/more/Check.java
@@ -6,68 +6,22 @@
         import android.widget.Toast;
 
         public class Check {
-            public boolean ISNull(String name, String kolovo, String a, String b, String tempstart, String tempend,String woltag,String osvet,String holost, Context context){
+            public boolean ISNull(double weight, double height, int age, Context context){
                 boolean trus = true;
-                if (name.equals("")){
-                    Toast.makeText(context,"Пусте поле!!\nНазвание",Toast.LENGTH_LONG).show();
+                if (weight>300&&weight<30){
+                    Toast.makeText(context,"Не правильний діапазон ваги",Toast.LENGTH_LONG).show();
                     trus = false;}
                 else {
-                    if (kolovo.equals("")) {
-                        Toast.makeText(context, "Пусте поле!!\nКількість панелей", Toast.LENGTH_LONG).show();
+                    if (height>270&&weight<70) {
+                        Toast.makeText(context, "Не правильний діапазон зросту", Toast.LENGTH_LONG).show();
                         trus = false;
                     } else {
-                        if (a.equals("")) {
-                            Toast.makeText(context, "Пусте поле!!\nСумарна довжина панелей", Toast.LENGTH_LONG).show();
+                        if (age>120&&age<0) {
+                            Toast.makeText(context, "Не правильний діапазон років", Toast.LENGTH_LONG).show();
                             trus = false;
-                        } else {
-                            if (b.equals("")) {
-                                Toast.makeText(context, "Пусте поле!!\nСумарна ширина панелей", Toast.LENGTH_LONG).show();
-                                trus = false;
-                            } else {
-                                if (tempstart.equals("")) {
-                                    Toast.makeText(context, "Пусте поле!!\nПочаткова температура ", Toast.LENGTH_LONG).show();
-                                    trus = false;
-                                } else {
-                                    if (tempend.equals("")) {
-                                        Toast.makeText(context, "Пусте поле!!\nКінцева температура ", Toast.LENGTH_LONG).show();
-                                        trus = false;
-                                    } else {
-                                        if (woltag.equals("")) {
-                                            Toast.makeText(context, "Пусте поле!!\nВольтаж", Toast.LENGTH_LONG).show();
-                                            trus = false;
-                                        } else {
-                                            if (holost.equals("")) {
-                                                Toast.makeText(context, "Пусте поле!!\nЧас зарядки акумулятора", Toast.LENGTH_LONG).show();
-                                                trus = false;
-                                            } else if (Float.parseFloat(holost) < 60) {
-                                                Toast.makeText(context, "час повинен бути більше години чи рівен їй!!\n час зарядки акумулятора", Toast.LENGTH_LONG).show();
-                                                trus = false;
-                                            } else if (Float.parseFloat(woltag) < 100) {
-                                                Toast.makeText(context, "Вольтаж повинен бути більшим сотні!!\nВольтаж", Toast.LENGTH_LONG).show();
-                                                trus = false;
-                                            } else {
-                                                if (osvet.equals("")) {
-                                                    Toast.makeText(context, "Пустое поле!!\nОсвітленість", Toast.LENGTH_LONG).show();
-                                                    trus = false;
-                                                } else {
-                                                    if (Float.parseFloat(kolovo) > 100) {
-                                                        Toast.makeText(context, "Кількість повинне бути меньше 100!!!!\nКількість", Toast.LENGTH_LONG).show();
-                                                        trus = false;
-                                                    } else {
-                                                        if (Float.parseFloat(tempend) > 51) {
-                                                            Toast.makeText(context, "Температура повинна бути меньше 51 градуса!!!!\nТемпература", Toast.LENGTH_LONG).show();
-                                                            trus = false;
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-
-                                    }
-                                }
-                            }
                         }
-                    }}
+                        }
+                    }
                 return trus;
             }
             public String Regu(String in){
