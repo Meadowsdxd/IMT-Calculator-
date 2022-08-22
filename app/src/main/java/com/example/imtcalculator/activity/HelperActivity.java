32d0993999ccc65d5e896eabc4214fbabec4825f
Meadowsdxd <58809634+Meadowsdxd@users.noreply.github.com>
2022-08-22 21:22:35 +0300

    Add New Link for weight loss

--- app/src/main/java/com/example/imtcalculator/activity/HelperActivity.java
+++ app/src/main/java/com/example/imtcalculator/activity/HelperActivity.java
@@ -19,8 +19,8 @@ public class HelperActivity extends AppCompatActivity{
 
         MyCardData[] myCardData = new MyCardData[]{
                 new MyCardData("https://wowbody.com","Прагнення до здорового, красивого та яскравого способу життя. Тренуй тіло, впроваджуй нові звички, готуй корисну страву та харчуйся із задоволенням. Будь WOW!",R.drawable.wowbody),
-                new MyCardData("Зхуднення 2","Зхуднення 2",R.drawable.men),
-                new MyCardData("Зхуднення 3","Зхуднення 3",R.drawable.men),
+                new MyCardData("https://hochu.ua/cat-health/diet-and-nutrition/article-64731-dieta-dlya-lenivyih-kakie-pravila-pomogut-sbrosit-lishney-ves-bez-usiliy/","Дієта для лінивих: які правила допоможуть скинути зайву вагу без зусиль",R.drawable.img),
+                new MyCardData("https://www.youtube.com/watch?v=hduA_n3_qMc","Как похудеть на 30 кг. Моя история похудения. Жиросжигатель",R.drawable.img_1),
                 new MyCardData("Зхуднення 4","Зхуднення 4",R.drawable.men),
                 new MyCardData("Зхуднення 5","Зхуднення 1",R.drawable.men),
                 new MyCardData("Зхуднення 6" ,"Зхуднення 1",R.drawable.men),
