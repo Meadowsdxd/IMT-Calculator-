3f0c369c5b1684952b424633025456a15e20036f
Meadowsdxd <58809634+Meadowsdxd@users.noreply.github.com>
2022-08-23 22:36:05 +0300

    Do settings for Cards

--- app/src/main/java/com/example/imtcalculator/activity/HelperActivity.java
+++ app/src/main/java/com/example/imtcalculator/activity/HelperActivity.java
@@ -21,16 +21,8 @@ public class HelperActivity extends AppCompatActivity{
         MyCardData[] myCardData = new MyCardData[0];
       Bundle arguments = getIntent().getExtras();
          result = (double) arguments.get("result");
-        myCardData = new MyCardData[]{
-                new MyCardData("https://wowbody.com", "Прагнення до здорового, красивого та яскравого способу життя. Тренуй тіло, впроваджуй нові звички, готуй корисну страву та харчуйся із задоволенням. Будь WOW!", R.drawable.wowbody),
-                new MyCardData("https://hochu.ua/cat-health/diet-and-nutrition/article-64731-dieta-dlya-lenivyih-kakie-pravila-pomogut-sbrosit-lishney-ves-bez-usiliy/", "Дієта для лінивих: які правила допоможуть скинути зайву вагу без зусиль", R.drawable.img),
-                new MyCardData("https://www.youtube.com/watch?v=hduA_n3_qMc", "Как похудеть на 30 кг. Моя история похудения. Жиросжигатель", R.drawable.img_1),
-                new MyCardData("Зхуднення 4", "Зхуднення 4", R.drawable.men),
-                new MyCardData("Зхуднення 5", "Зхуднення 1", R.drawable.men),
-                new MyCardData("Зхуднення 6", "Зхуднення 1", R.drawable.men),
-                new MyCardData("Зхуднення 7", "Зхуднення 1", R.drawable.men),
-        };
-      /* if(result<18){ myCardData = new MyCardData[]{
+
+       if(result<18){ myCardData = new MyCardData[]{
                 new MyCardData("https://wowbody.com", "Прагнення до здорового, красивого та яскравого способу життя. Тренуй тіло, впроваджуй нові звички, готуй корисну страву та харчуйся із задоволенням. Будь WOW!", R.drawable.wowbody),
                 new MyCardData("https://hochu.ua/cat-health/diet-and-nutrition/article-64731-dieta-dlya-lenivyih-kakie-pravila-pomogut-sbrosit-lishney-ves-bez-usiliy/", "Дієта для лінивих: які правила допоможуть скинути зайву вагу без зусиль", R.drawable.img),
                 new MyCardData("https://www.youtube.com/watch?v=hduA_n3_qMc", "Как похудеть на 30 кг. Моя история похудения. Жиросжигатель", R.drawable.img_1),
@@ -89,7 +81,7 @@ public class HelperActivity extends AppCompatActivity{
             Toast.makeText(this,"Data is empty",Toast.LENGTH_LONG).show();
 
 
-        }*/
+        }
         MyCardAdapter myCardAdapter = new MyCardAdapter(myCardData,HelperActivity.this);
         recyclerView.setAdapter(myCardAdapter);
     }
