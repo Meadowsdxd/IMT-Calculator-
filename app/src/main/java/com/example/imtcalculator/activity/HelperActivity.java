e930c4180f1db4333627b12e46b3b4a94b439265
Meadowsdxd <58809634+Meadowsdxd@users.noreply.github.com>
2022-08-23 22:42:48 +0300

    Do settings for Cards

--- app/src/main/java/com/example/imtcalculator/activity/HelperActivity.java
+++ app/src/main/java/com/example/imtcalculator/activity/HelperActivity.java
@@ -23,9 +23,9 @@ public class HelperActivity extends AppCompatActivity{
          result = (double) arguments.get("result");
 
        if(result<18){ myCardData = new MyCardData[]{
-                new MyCardData("https://wowbody.com", "Прагнення до здорового, красивого та яскравого способу життя. Тренуй тіло, впроваджуй нові звички, готуй корисну страву та харчуйся із задоволенням. Будь WOW!", R.drawable.wowbody),
-                new MyCardData("https://hochu.ua/cat-health/diet-and-nutrition/article-64731-dieta-dlya-lenivyih-kakie-pravila-pomogut-sbrosit-lishney-ves-bez-usiliy/", "Дієта для лінивих: які правила допоможуть скинути зайву вагу без зусиль", R.drawable.img),
-                new MyCardData("https://www.youtube.com/watch?v=hduA_n3_qMc", "Как похудеть на 30 кг. Моя история похудения. Жиросжигатель", R.drawable.img_1),
+                new MyCardData("https://www.youtube.com/watch?v=4s3QStdR-HU", "КАК ПИТАТЬСЯ, ЧТОБЫ НАБРАТЬ МЫШЕЧНУЮ МАССУ! 7 ЗАКОНОВ", R.drawable.img),
+                new MyCardData("https://www.youtube.com/watch?v=k9nlPis5u7s", "10 ГЛАВНЫХ ОШИБОК ПРИ НАБОРЕ МАССЫ", R.drawable.img_3),
+                new MyCardData("https://www.youtube.com/watch?v=hduA_n3_qMc", "Как похудеть на 30 кг. Моя история похудения. Жиросжигатель", R.drawable.img_3),
                 new MyCardData("Зхуднення 4", "Набор 4", R.drawable.men),
                 new MyCardData("Зхуднення 5", "Набор 1", R.drawable.men),
                 new MyCardData("Зхуднення 6", "Набор 1", R.drawable.men),
@@ -34,8 +34,8 @@ public class HelperActivity extends AppCompatActivity{
         if(result>=18.5&&result<25) {
              myCardData = new MyCardData[]{
                     new MyCardData("https://wowbody.com", "Прагнення до здорового, красивого та яскравого способу життя. Тренуй тіло, впроваджуй нові звички, готуй корисну страву та харчуйся із задоволенням. Будь WOW!", R.drawable.wowbody),
-                    new MyCardData("https://hochu.ua/cat-health/diet-and-nutrition/article-64731-dieta-dlya-lenivyih-kakie-pravila-pomogut-sbrosit-lishney-ves-bez-usiliy/", "Дієта для лінивих: які правила допоможуть скинути зайву вагу без зусиль", R.drawable.img),
-                    new MyCardData("https://www.youtube.com/watch?v=hduA_n3_qMc", "Как похудеть на 30 кг. Моя история похудения. Жиросжигатель", R.drawable.img_1),
+                    new MyCardData("https://hochu.ua/cat-health/diet-and-nutrition/article-64731-dieta-dlya-lenivyih-kakie-pravila-pomogut-sbrosit-lishney-ves-bez-usiliy/", "Дієта для лінивих: які правила допоможуть скинути зайву вагу без зусиль", R.drawable.img_2),
+                    new MyCardData("https://www.youtube.com/watch?v=hduA_n3_qMc", "Как похудеть на 30 кг. Моя история похудения. Жиросжигатель", R.drawable.img_3),
                     new MyCardData("Зхуднення 4", "Підтримка 4", R.drawable.men),
                     new MyCardData("Зхуднення 5", "Підтримка 1", R.drawable.men),
                     new MyCardData("Зхуднення 6", "Підтримка 1", R.drawable.men),
@@ -44,8 +44,8 @@ public class HelperActivity extends AppCompatActivity{
         }else if(result>=25&&result<30) {
             myCardData = new MyCardData[]{
                     new MyCardData("https://wowbody.com", "Прагнення до здорового, красивого та яскравого способу життя. Тренуй тіло, впроваджуй нові звички, готуй корисну страву та харчуйся із задоволенням. Будь WOW!", R.drawable.wowbody),
-                    new MyCardData("https://hochu.ua/cat-health/diet-and-nutrition/article-64731-dieta-dlya-lenivyih-kakie-pravila-pomogut-sbrosit-lishney-ves-bez-usiliy/", "Дієта для лінивих: які правила допоможуть скинути зайву вагу без зусиль", R.drawable.img),
-                    new MyCardData("https://www.youtube.com/watch?v=hduA_n3_qMc", "Как похудеть на 30 кг. Моя история похудения. Жиросжигатель", R.drawable.img_1),
+                    new MyCardData("https://hochu.ua/cat-health/diet-and-nutrition/article-64731-dieta-dlya-lenivyih-kakie-pravila-pomogut-sbrosit-lishney-ves-bez-usiliy/", "Дієта для лінивих: які правила допоможуть скинути зайву вагу без зусиль", R.drawable.img_2),
+                    new MyCardData("https://www.youtube.com/watch?v=hduA_n3_qMc", "Как похудеть на 30 кг. Моя история похудения. Жиросжигатель", R.drawable.img_3),
                     new MyCardData("Зхуднення 4", "Підтримка 4", R.drawable.men),
                     new MyCardData("Зхуднення 5", "Підтримка 1", R.drawable.men),
                     new MyCardData("Зхуднення 6", "Підтримка 1", R.drawable.men),
@@ -53,8 +53,8 @@ public class HelperActivity extends AppCompatActivity{
             };}else if(result>=30&&result<35) {
             myCardData = new MyCardData[]{
                     new MyCardData("https://wowbody.com", "Прагнення до здорового, красивого та яскравого способу життя. Тренуй тіло, впроваджуй нові звички, готуй корисну страву та харчуйся із задоволенням. Будь WOW!", R.drawable.wowbody),
-                    new MyCardData("https://hochu.ua/cat-health/diet-and-nutrition/article-64731-dieta-dlya-lenivyih-kakie-pravila-pomogut-sbrosit-lishney-ves-bez-usiliy/", "Дієта для лінивих: які правила допоможуть скинути зайву вагу без зусиль", R.drawable.img),
-                    new MyCardData("https://www.youtube.com/watch?v=hduA_n3_qMc", "Как похудеть на 30 кг. Моя история похудения. Жиросжигатель", R.drawable.img_1),
+                    new MyCardData("https://hochu.ua/cat-health/diet-and-nutrition/article-64731-dieta-dlya-lenivyih-kakie-pravila-pomogut-sbrosit-lishney-ves-bez-usiliy/", "Дієта для лінивих: які правила допоможуть скинути зайву вагу без зусиль", R.drawable.img_2),
+                    new MyCardData("https://www.youtube.com/watch?v=hduA_n3_qMc", "Как похудеть на 30 кг. Моя история похудения. Жиросжигатель", R.drawable.img_3),
                     new MyCardData("Зхуднення 4", "Зхуднення 4", R.drawable.men),
                     new MyCardData("Зхуднення 5", "Зхуднення 1", R.drawable.men),
                     new MyCardData("Зхуднення 6", "Зхуднення 1", R.drawable.men),
@@ -62,8 +62,8 @@ public class HelperActivity extends AppCompatActivity{
             };}else if(result>=35&&result<40) {
             myCardData = new MyCardData[]{
                     new MyCardData("https://wowbody.com", "Прагнення до здорового, красивого та яскравого способу життя. Тренуй тіло, впроваджуй нові звички, готуй корисну страву та харчуйся із задоволенням. Будь WOW!", R.drawable.wowbody),
-                    new MyCardData("https://hochu.ua/cat-health/diet-and-nutrition/article-64731-dieta-dlya-lenivyih-kakie-pravila-pomogut-sbrosit-lishney-ves-bez-usiliy/", "Дієта для лінивих: які правила допоможуть скинути зайву вагу без зусиль", R.drawable.img),
-                    new MyCardData("https://www.youtube.com/watch?v=hduA_n3_qMc", "Как похудеть на 30 кг. Моя история похудения. Жиросжигатель", R.drawable.img_1),
+                    new MyCardData("https://hochu.ua/cat-health/diet-and-nutrition/article-64731-dieta-dlya-lenivyih-kakie-pravila-pomogut-sbrosit-lishney-ves-bez-usiliy/", "Дієта для лінивих: які правила допоможуть скинути зайву вагу без зусиль", R.drawable.img_2),
+                    new MyCardData("https://www.youtube.com/watch?v=hduA_n3_qMc", "Как похудеть на 30 кг. Моя история похудения. Жиросжигатель", R.drawable.img_3),
                     new MyCardData("Зхуднення 4", "Зхуднення 4", R.drawable.men),
                     new MyCardData("Зхуднення 5", "Зхуднення 1", R.drawable.men),
                     new MyCardData("Зхуднення 6", "Зхуднення 1", R.drawable.men),
@@ -71,8 +71,8 @@ public class HelperActivity extends AppCompatActivity{
             };}else if(result>=40) {
             myCardData = new MyCardData[]{
                     new MyCardData("https://wowbody.com", "Прагнення до здорового, красивого та яскравого способу життя. Тренуй тіло, впроваджуй нові звички, готуй корисну страву та харчуйся із задоволенням. Будь WOW!", R.drawable.wowbody),
-                    new MyCardData("https://hochu.ua/cat-health/diet-and-nutrition/article-64731-dieta-dlya-lenivyih-kakie-pravila-pomogut-sbrosit-lishney-ves-bez-usiliy/", "Дієта для лінивих: які правила допоможуть скинути зайву вагу без зусиль", R.drawable.img),
-                    new MyCardData("https://www.youtube.com/watch?v=hduA_n3_qMc", "Как похудеть на 30 кг. Моя история похудения. Жиросжигатель", R.drawable.img_1),
+                    new MyCardData("https://hochu.ua/cat-health/diet-and-nutrition/article-64731-dieta-dlya-lenivyih-kakie-pravila-pomogut-sbrosit-lishney-ves-bez-usiliy/", "Дієта для лінивих: які правила допоможуть скинути зайву вагу без зусиль", R.drawable.img_2),
+                    new MyCardData("https://www.youtube.com/watch?v=hduA_n3_qMc", "Как похудеть на 30 кг. Моя история похудения. Жиросжигатель", R.drawable.img_3),
                     new MyCardData("Зхуднення 4", "Зхуднення 4", R.drawable.men),
                     new MyCardData("Зхуднення 5", "Зхуднення 1", R.drawable.men),
                     new MyCardData("Зхуднення 6", "Зхуднення 1", R.drawable.men),
