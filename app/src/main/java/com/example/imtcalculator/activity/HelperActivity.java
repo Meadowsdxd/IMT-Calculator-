b32add7d4e96736235d8b9764c3ddf0d362df3fa
Meadowsdxd <58809634+Meadowsdxd@users.noreply.github.com>
2022-08-25 21:34:01 +0300

    Add two more link for helping

--- app/src/main/java/com/example/imtcalculator/activity/HelperActivity.java
+++ app/src/main/java/com/example/imtcalculator/activity/HelperActivity.java
@@ -23,19 +23,19 @@ public class HelperActivity extends AppCompatActivity{
          result = (double) arguments.get("result");
 
        if(result<18){ myCardData = new MyCardData[]{
-                new MyCardData("https://www.youtube.com/watch?v=4s3QStdR-HU", "КАК ПИТАТЬСЯ, ЧТОБЫ НАБРАТЬ МЫШЕЧНУЮ МАССУ! 7 ЗАКОНОВ", R.drawable.img),
+                new MyCardData("https://www.youtube.com/watch?v=4s3QStdR-HU", "КАК ПИТАТЬСЯ, ЧТОБЫ НАБРАТЬ МЫШЕЧНУЮ МАССУ! 7 ЗАКОНОВ", R.drawable.img_1),
                 new MyCardData("https://www.youtube.com/watch?v=k9nlPis5u7s", "10 ГЛАВНЫХ ОШИБОК ПРИ НАБОРЕ МАССЫ", R.drawable.img_3),
                 new MyCardData("https://www.youtube.com/watch?v=hduA_n3_qMc", "Как похудеть на 30 кг. Моя история похудения. Жиросжигатель", R.drawable.img_3),
                 new MyCardData("https://fitness.org.ua/iak-shvidko-nabrati-vagy-prikladi-menu-prodyktiv-i-vprav/", "Ви завжди вважали себе занадто худим? Шкіра та кістки? В той час, коли більшості необхідно скинути вагу, набрати його може бути дуже складним завданням.\n"+
                         "Хочете дізнатися як швидко набрати вагу без шкоди для здоров’я? У цій статті ми розповімо як збільшити масу тіла за короткий термін.", R.drawable.img_5),
                 new MyCardData("https://bodia.online/subcategory/sportivni-zali-sektsiyi/trenazhernii-zal-ta-fitnes", "ТРЕНАЖЕРНІ ЗАЛИ ТА ФІТНЕС КЛУБИ В УКРАЇНІ", R.drawable.img_4),
-                new MyCardData("Зхуднення 6", "Набор 1", R.drawable.men),
-                new MyCardData("Зхуднення 7", "Набор 1", R.drawable.men),
+             /*   new MyCardData("Зхуднення 6", "Набор 1", R.drawable.men),
+                new MyCardData("Зхуднення 7", "Набор 1", R.drawable.men),*/
         };}else
         if(result>=18.5&&result<25) {
              myCardData = new MyCardData[]{
-                    new MyCardData("https://wowbody.com", "Прагнення до здорового, красивого та яскравого способу життя. Тренуй тіло, впроваджуй нові звички, готуй корисну страву та харчуйся із задоволенням. Будь WOW!", R.drawable.wowbody),
-                    new MyCardData("https://hochu.ua/cat-health/diet-and-nutrition/article-64731-dieta-dlya-lenivyih-kakie-pravila-pomogut-sbrosit-lishney-ves-bez-usiliy/", "Дієта для лінивих: які правила допоможуть скинути зайву вагу без зусиль", R.drawable.img_2),
+                    new MyCardData("https://gymmaxx.com", "Фітнес клуб преміум клас", R.drawable.gymaxx),
+                    new MyCardData("https://sportzal.com.ua/ru", "Интернет-магазин спортивных тренажеров SPORTZAL", R.drawable.sportazal),
                     new MyCardData("https://www.youtube.com/watch?v=hduA_n3_qMc", "Как похудеть на 30 кг. Моя история похудения. Жиросжигатель", R.drawable.img_3),
                     new MyCardData("Зхуднення 4", "Підтримка 4", R.drawable.men),
                     new MyCardData("Зхуднення 5", "Підтримка 1", R.drawable.men),
