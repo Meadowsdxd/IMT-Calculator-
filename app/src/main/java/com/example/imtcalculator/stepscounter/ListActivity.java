9823dec41321168027867c99ad28b48500e8ebdc
Meadowsdxd <58809634+Meadowsdxd@users.noreply.github.com>
2022-08-31 18:52:55 +0300

    connect firebase to project

--- app/src/main/java/com/example/imtcalculator/stepscounter/ListActivity.java
+++ app/src/main/java/com/example/imtcalculator/stepscounter/ListActivity.java
@@ -34,6 +34,7 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        init();
     }
 
     private void init() {
